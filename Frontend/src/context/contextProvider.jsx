45e1bc8562ad01e269e04ae98a2a46e0843c1c8d
MarceloOrtegaDev <ortegamarcelodavid77@gmail.com>
2024-11-01 02:30:22 -0300

    Se modularizó el context con un Custom hook, se agregaron las validaciones y alertas al formulario de inicio de session

--- Frontend/src/context/contextProvider.jsx
+++ Frontend/src/context/contextProvider.jsx
@@ -1,58 +1,19 @@
-import React, { createContext, useContext, useReducer, useEffect } from 'react';
-import { login, logoutUser, sessionUser} from '../services/userService';
-import { reducer } from '../reducer/reducer.js';
-import { initialState } from '../reducer/states.js';
-import { authTypes } from '../reducer/types.js';
+import React, { createContext, useContext} from 'react';
+import { useSession } from '../Hooks/sessionHook.js';
 
 export const loginContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const loginUser = async (email, password) => {
-        const response = await login(email, password);
-        if (response.ok) {
-            dispatch({ type: authTypes.Logged, payload: response });
-        } else {
-            console.error("Error en el login");
-        }
-    };
-
-    const logout = async () => {
-        try {
-            const response = await logoutUser();
-            if (response.ok) {
-                dispatch({type: authTypes.loggedOut});
-            }
-        } catch (error) {
-            console.error("Error al cerrar sesión:", error);
-        }
-    };
-
-    const useSession = async () => {
-        const res = await sessionUser();
-        if (res.ok) {
-            dispatch({ type: authTypes.Logged, payload: res }); // Actualiza el estado
-        } else {
-            console.log("Error en la sesión");
-            dispatch({ type: authTypes.loggedOut }); // Actualiza el estado si no está autenticado
-        }
-    };
-
-    // Verifica la sesión al cargar el contexto
-    useEffect(() => {
-        useSession(); // Llama a useSession al cargar el contexto
-    }, []);
     
+const {loginUser, logout, getSession, state} = useSession()
 
 
     return (
-        <loginContext.Provider value={{ state, loginUser, logout}}>
+        <loginContext.Provider value={{ state, loginUser, logout, getSession}}>
             {children}
         </loginContext.Provider>
     );
 };
 
-// Ajuste aquí para que `authenticate` no sea async ni use paréntesis
+
 export const authenticate = () => useContext(loginContext);
